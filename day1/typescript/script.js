c27f578869718923d453db5ca05ef45973f5c776
Dinesh Mamidi <dinesh.k.mamidi@gmail.com>
2018-07-07 12:25:37 -0400

    july 7 checkin

---
+++ day1/typescript/script.js
@@ -0,0 +1,3 @@
+var name1 = 'Dinesh';
+var msg = "Hello " + name1 + "! How are you?";
+console.log(msg);
---
+++ day1/typescript/script.ts
@@ -0,0 +1,3 @@
+let name2: string = 'Dinesh';
+let msg1: string = `Hello ${name2}! How are you?`;
+console.log(msg1);
